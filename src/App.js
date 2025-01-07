fbcafcce5423b772ffb2aa49f3af4119a47c7054
hungnq131002 <hungnq131002@gmail.com>
2025-01-07 15:31:49 +0700

    properties of React in JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+const Person = (props) => {
+  return (
+    <>
+      <h1> Name: {props.name}</h1>
+      <h1> Last Name: {props.lastName}</h1>
+      <h1> Age: {props.age}</h1>
+    </>
+  )
+}
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Person name="John" lastName={'Doe'}/>
+      <Person />
+      <Person />
+      <Person />
     </div>
   );
 }
